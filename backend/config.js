c66c09a43b2c6136bea7a838ab90e1a80df8ce4c
Aliabuhassan2002 <ali.mohammad.abuhassan.se@gmail.com>
2025-02-25 10:00:22 +0300

    New secured code

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 // PostgreSQL Connection Pool
 const pool = new Pool({
-  user: "postgres", // Change this
-  host: "localhost",
-  password: "aaallliii@2002", // Change this
-  database: "book_catalog", // Change this
+  user: process.env.DB_user,
+  host: process.env.DB_host,
+  password: process.env.DB_password, // Change this
+  database: process.env.DB_databaseName, // Change this
   port: 5432, // Default PostgreSQL port
 });
 
